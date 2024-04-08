034357e74623f4398e10256a99652562c1fc414f
Prithvi <prithvia28@gmail.com>
2024-04-08 05:15:11 -0400

    Updated

---
+++ src/main/java/edu/primesservice/controller/AuthenticationController.java
@@ -0,0 +1,52 @@
+package edu.primesservice.controller;
+
+import edu.primesservice.model.Customer;
+import edu.primesservice.service.AuthenticationService;
+import edu.primesservice.service.IAuthenticationService;
+import edu.primesservice.service.TokenService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class AuthenticationController {
+
+    private final IAuthenticationService authenticationService;
+    private final AuthenticationManager authenticationManager;
+    private TokenService tokenService;
+
+    public AuthenticationController( AuthenticationManager authenticationManager,
+            IAuthenticationService authenticationService,
+                                     TokenService tokenService) {
+        this.authenticationManager = authenticationManager;
+        this.authenticationService = authenticationService;
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/register")
+    public boolean register(@RequestBody Customer customer) {
+        try{
+            return authenticationService.register(customer);
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody Customer customer){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        customer.getUsername(),customer.getPassword()
+                )
+        );
+        return tokenService.generateToken(authentication);
+    }
+
+}
---
+++ src/main/java/edu/primesservice/service/IAuthenticationService.java
@@ -0,0 +1,11 @@
+package edu.primesservice.service;
+
+import edu.primesservice.model.Customer;
+
+import java.io.IOException;
+
+public interface IAuthenticationService {
+
+    boolean register(Customer customer) throws IOException;
+    boolean login(String username,String password) throws IOException;
+}
